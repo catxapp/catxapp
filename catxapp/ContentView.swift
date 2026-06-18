04cf1579a7ca1cee3f7a120be121c33e85e8acc5
Lukas <quantumficial@icloud.com>
2026-06-17 17:37:07 -0700

    Initial Commit

---
+++ catxapp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  catxapp
+//
+//  Created by Lukas on 6/17/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ catxapp/catxappApp.swift
@@ -0,0 +1,17 @@
+//
+//  catxappApp.swift
+//  catxapp
+//
+//  Created by Lukas on 6/17/26.
+//
+
+import SwiftUI
+
+@main
+struct catxappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
